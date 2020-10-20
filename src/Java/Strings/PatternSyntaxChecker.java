592a00d317f92f985c64849d5c5d690facf14b2d
Kawthar Al-Tamimi <kawther.mujahed@mawdoo3.com>
2020-10-20 18:34:37 +0300

    PatternSyntaxChecker solution

--- src/Java/Strings/PatternSyntaxChecker.java
+++ src/Java/Strings/PatternSyntaxChecker.java
@@ -1,6 +1,26 @@
 package Java.Strings;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class PatternSyntaxChecker {
 
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        int i = 0;
+        while(testCases > i){
+            String pattern = in.nextLine();
+            //Write your code
+           try {
+               Pattern.compile(pattern);
+               System.out.println("Valid");
 
+           } catch (PatternSyntaxException error) {
+               System.out.println("Invalid");
+           }
+            i++;
+        }
+    }
 }
