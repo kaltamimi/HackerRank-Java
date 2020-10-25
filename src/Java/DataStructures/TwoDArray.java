7c8bb0a19e09181d80cf5abc138a1574bfd727d6
Kawthar Al-Tamimi <kawther.mujahed@mawdoo3.com>
2020-10-25 22:16:56 +0300

    Java 2D Array Solution

---
+++ src/Java/DataStructures/TwoDArray.java
@@ -0,0 +1,37 @@
+package Java.DataStructures;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        scanner.close();
+
+        int largestSum = Integer.MIN_VALUE;
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                int firsRowSum = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                int secondRowSum = arr[i+1][j+1];
+                int thirdRowSum = arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                int total_sum = firsRowSum + secondRowSum + thirdRowSum;
+
+                largestSum = Math.max(largestSum, total_sum);
+            }
+        }
+        System.out.println(largestSum);
+    }
+
+}
