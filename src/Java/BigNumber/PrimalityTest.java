651c65da23572926918073d6be9d41f8952c90d7
Kawthar Al-Tamimi <kawther.mujahed@mawdoo3.com>
2020-11-04 13:05:34 +0200

    Java Primality Test Solution

---
+++ src/Java/BigNumber/PrimalityTest.java
@@ -0,0 +1,21 @@
+package Java.BigNumber;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class PrimalityTest {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+
+        scanner.close();
+
+        BigInteger number = new BigInteger(n);
+
+        System.out.println(number.isProbablePrime(10) ? "prime" : "not prime");
+
+    }
+
+}
