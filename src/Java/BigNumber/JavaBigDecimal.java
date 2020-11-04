2d1bf848d5c14c4951921eaafbf8160af03d8583
Kawthar Al-Tamimi <kawther.mujahed@mawdoo3.com>
2020-11-05 00:45:39 +0200

    Java BigDecimal Solution

---
+++ src/Java/BigNumber/JavaBigDecimal.java
@@ -0,0 +1,40 @@
+package Java.BigNumber;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class JavaBigDecimal {
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        //Write your code here
+        Arrays.sort(s, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1 == null || o2 == null) {
+                    return 0;
+                }
+
+                BigDecimal n1 = new BigDecimal(o1);
+                BigDecimal n2 = new BigDecimal(o2);
+                return n2.compareTo(n1);
+            }
+        });
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
